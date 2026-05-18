f44ce56b01bee4de69d2097251522b37f1c9f9bb
Miguel Mendonça <miguelbmendonca20@gmail.com>
2026-05-18 02:22:38 -0300

    Brief description of what changed

--- dist/assets/chatList-uBpjZuHl.js
+++ dist/assets/chatList-uBpjZuHl.js
@@ -1,4 +1,4 @@
-import{r as c,a as Qe,j as n}from"./vendor-react-D5rsd9ER.js";import{c as es,i as we,b as ts,Y as Ks,n as Qs,Z as Gs,S as Mt,a as Be,z as se,q as Xs,u as Ys,_ as nt,$ as Js,a0 as ei,A as ut,a1 as ti,a2 as ni,a3 as Pn,N as si,g as te,a4 as ht,y as Me,a5 as ii,a6 as On,T as ai,j as ri,a7 as ns,a8 as oi}from"./index-B6kNtksK.js";import{u as li,a as ci}from"./vendor-router-BNA5Ka0A.js";/**
+import{r as c,a as Qe,j as n}from"./vendor-react-D5rsd9ER.js";import{c as es,i as we,b as ts,Y as Ks,n as Qs,Z as Gs,S as Mt,a as Be,z as se,q as Xs,u as Ys,_ as nt,$ as Js,a0 as ei,A as ut,a1 as ti,a2 as ni,a3 as Pn,N as si,g as te,a4 as ht,y as Me,a5 as ii,a6 as On,T as ai,j as ri,a7 as ns,a8 as oi}from"./index-GJ3-q5HI.js";import{u as li,a as ci}from"./vendor-router-BNA5Ka0A.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
