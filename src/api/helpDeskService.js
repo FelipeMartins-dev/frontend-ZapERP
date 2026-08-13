e4bee132564ee72bfbd058ec88e4cf0f6ad67f8e
Felipe de Oliveira Martins <felps450@gmail.com>
2026-08-13 16:11:29 -0300

    Adiciona acoes de atendimento ao HelpDesk

--- src/api/helpDeskService.js
+++ src/api/helpDeskService.js
@@ -35,3 +35,8 @@ export async function transferTicket(id, payload) {
   const { data } = await api.post(`${BASE}/tickets/${id}/transfer`, payload)
   return data
 }
+
+export async function assumeTicket(id) {
+  const { data } = await api.post(`${BASE}/tickets/${id}/assume`)
+  return data
+}
